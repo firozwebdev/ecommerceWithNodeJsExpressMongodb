a554e6079863a4c8964391f71cf73429c60585fa
sabuzwebdev <frsmgt@gmail.com>
2018-04-28 12:49:23 +0600

    Admin page is created

---
+++ app.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var path = require('path');
+var mongoose = require('mongoose');
+var config = require('./config/database');
+var bodyParser = require('body-parser')
+var session = require('express-session')
+var expressValidator = require('express-validator');
+
+//connect to db
+mongoose.connect(config.database);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected to MongoDB');
+});
+
+//init app
+var app = express();
+
+//view engine setup
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+//set public folder
+app.use(express.static(path.join(__dirname,'public')));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+//Express Session
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }));
+
+// Express Validator middleware
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.')
+                , root = namespace.shift()
+                , formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+
+}));
+
+//Express message Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//set routes
+var pages = require('./routes/pages.js');
+var adminpages = require('./routes/adminpages.js');
+
+app.use('/admin/pages',adminpages);
+app.use('/',pages);
+
+//start server
+var port = 3000;
+app.listen(port,function(){
+    console.log("Server starting " +port );
+});
---
+++ routes/adminpages.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/',function(req, res){
+    res.render('index',{
+        title: 'Admin Home'
+    });
+});
+
+//Get add page
+router.get('/add-page',function(req, res){
+   var title = "";
+   var slug = "";
+   var content = "";
+
+   res.render('admin/add_page',{
+       title: title,
+       slug: slug,
+       content: content,
+   });
+});
+
+//Post add page
+router.post('/add-page', function (req, res) {
+
+    req.checkBody('title', 'Title must have a value.').notEmpty();
+    req.checkBody('content', 'Content must have a value.').notEmpty();
+
+    var title = req.body.title;
+    var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
+    if (slug == "")
+        slug = title.replace(/\s+/g, '-').toLowerCase();
+    var content = req.body.content;
+
+    var errors = req.validationErrors();
+   
+    if (errors) {
+        console.log('errors');
+        res.render('admin/add_page', {
+            errors: errors,
+            title: title,
+            slug: slug,
+            content: content
+        });
+    } else {
+        console.log('success');
+    }
+
+ 
+});
+
+
+//Exports
+module.exports = router;
