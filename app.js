736623f13c0facc4643b28e27f96d0a716776335
sabuzwebdev <frsmgt@gmail.com>
2018-05-07 12:53:51 +0600

    Pages are displayed in Front site

--- app.js
+++ app.js
@@ -28,6 +28,18 @@ app.use(express.static(path.join(__dirname,'public')));
 //Set global errors variable
 app.locals.errors = null;
 
+//Get page Model
+var Page = require('./models/page');
+
+//Get all pages to pass header.ejs
+Page.find({}).sort({sorting: 1}).exec(function(err, pages){
+    if(err) {
+        console.log(err)
+    }else{
+        app.locals.pages = pages;
+    }
+});
+
 // Express fileUpload middleware
 app.use(fileUpload());
 
