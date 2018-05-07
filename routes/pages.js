d8e5556503acbfff8e7134ef176bcc242d9a06e4
sabuzwebdev <frsmgt@gmail.com>
2018-05-07 13:35:58 +0600

    content are displayed in front page

--- routes/pages.js
+++ routes/pages.js
@@ -1,12 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+
+//Load models
+Page = require('../models/page');
+
+//Get Index page
 router.get('/',function(req, res){
-    res.render('index',{
-        title: 'Home'
+    Page.findOne({slug: 'home'}, function(err,page){
+        if(err) console.log(err);
+        
+        res.render('index',{
+            title: page.title,
+            content: page.content
+        });
+        
     });
 });
 
+//get a page
+router.get('/:slug',function(req,res){
+    var slug = req.params.slug;
+    Page.findOne({slug: slug}, function(err,page){
+        if(err) console.log(err);
+        if(!page){
+            res.redirect('/');
+        }else{
+            res.render('index',{
+                title: page.title,
+                content: page.content
+            });
+        }
+    });
+    
+});
 
 //Exports
 module.exports = router;
